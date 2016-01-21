97a5b6b474f3d33a8866dd4a151cfc706a27dacf
QuentinAndre <quentin.andre@insead.edu>
2016-01-21 17:40:56 +0100

    Refactored.

--- Instance/config.py
+++ Instance/config.py
@@ -1,9 +1,3 @@
-import os
-APP_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-# Basic configuration
-DEBUG = True
-SECRET_KEY = "devkey"
-USERNAME = "admin"
-PASSWORD = "default"
+
 
 
